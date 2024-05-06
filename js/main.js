e9ad8044d88a03394540b23923076dd09393067f
dmitrii_mishchenko <dmittry.m@gmail.com>
2024-05-06 15:20:24 +0300

    adaptivity

--- js/main.js
+++ js/main.js
@@ -1,26 +1,63 @@
 import { openModal } from "./functons.js";
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
-const headerListButton = document.getElementById('header-lists');
-const blackListButton = document.querySelector('.black-list-button');
-const whiteListButton = document.querySelector('.white-list-button');
+function toggle(burger, nav, animation) {
+    if (!animation) {
+        nav.style.transition = 'transform 300ms ease-in-out';
+        burger.classList.toggle('burger--active');
+        nav.classList.toggle('header-nav--active');
+        animationStop = true;
+        setTimeout(() => {
+            animationStop = false;
+        }, 200)
+    }
+}
+
 const swiperBlock = document.querySelector('.swiper__swiper');
+const headerBurgerButton = document.querySelector('.header__burger');
+const burgerNav = document.querySelector('.header__nav');
+const burgerNavLink = document.querySelectorAll('.header__nav-link');
+let animationStop = false;
+let slides = 0;
 
-blackListButton.addEventListener('click', () => {
-    openModal(true);
+if (screen.availWidth > 1200) {
+    slides = 5;
+} else if (screen.availWidth > 768) {
+    slides = 4;
+} else if (screen.availWidth > 768) {
+    slides = 3;
+} else {
+    slides = 1;
+}
+
+document.addEventListener('click', (e) => {
+    console.log(screen.availWidth > 5)
+    if (headerBurgerButton.classList.contains('burger--active') && !animationStop) {
+        if (!burgerNav.contains(e.target)) {
+            toggle(headerBurgerButton, burgerNav, animationStop);
+        }
+    }
+});
+
+headerBurgerButton.addEventListener('click', () => {
+    toggle(headerBurgerButton, burgerNav, animationStop);
 })
 
-whiteListButton.addEventListener('click', () => {
-    openModal(false);
+burgerNav.addEventListener('transitionend', () => {
+    burgerNav.style.transition = '';
 })
 
-headerListButton.addEventListener('click', () => {
-    openModal(true)
-})
+burgerNavLink.forEach(elem => elem.addEventListener('click', () => {
+    if (headerBurgerButton.classList.contains('burger--active')) {
+        toggle(headerBurgerButton, burgerNav, animationStop);
+    }
+}))
+
+
 
 var swiper = new Swiper(swiperBlock, {
-    spaceBetween: 10,
-    slidesPerView: 6,
+    spaceBetween: 5,
+    slidesPerView: slides,
     loop: true,
     freeMode: true,
     autoplay: {
